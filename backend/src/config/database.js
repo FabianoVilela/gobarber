a32cc778ac32021498dd53bdf048a1e01974d98f
Fabiano Vilela <fabiano.vilela@gmail.com>
2019-06-29 13:36:26 -0300

    Sequilize setup

---
+++ backend/src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  port: '5433',
+  username: 'postgres',
+  password: 'docker',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
