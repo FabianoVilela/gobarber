dc67149f8abfc236fae72ce49d9bf8ff5e9be9d2
Fabiano Vilela <fabiano.vilela@gmail.com>
2019-07-01 23:17:58 -0300

    Validations

--- backend/src/app/controllers/SessionController.js
+++ backend/src/app/controllers/SessionController.js
@@ -1,19 +1,32 @@
 import jwt from 'jsonwebtoken';
+import * as Yup from 'yup';
 
 import User from '../models/User';
 import authConfig from '../../config/auth';
 
 class SessionController {
   async store(req, res) {
+    // Validação
+    const schema = Yup.object().shape({
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(401).json({ error: 'Validation fails' });
+    }
+
     const { email, password } = req.body;
 
     const user = await User.findOne({ where: { email }});
 
-    if(!user) {
+    if (!user) {
       return res.status(401).json({ error: 'User not found!' });
     }
 
-    if(await !user.checkPassword(password)) {
+    if (await !user.checkPassword(password)) {
       return res.status(401).json({ error: 'Password does not match!' });
     }
 
