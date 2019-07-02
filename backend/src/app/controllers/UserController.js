a07dafaf94a75cdd371e633c66c5476770e685b1
Fabiano Vilela <fabiano.vilela@gmail.com>
2019-07-01 21:51:27 -0300

    User update

--- backend/src/app/controllers/UserController.js
+++ backend/src/app/controllers/UserController.js
@@ -2,14 +2,39 @@ import User from '../models/User';
 
 class UserController {
   async store(req, res) {
-    const userEmail = req.body.email;
-    const userExists = await User.findOne({ where: { email: userEmail } });
+    const user = await User.findOne({ where: { email: req.body.email } });
 
-    if (userExists) {
+    if (user) {
       return res.status(400).json({ error: 'User already exists!' });
     }
 
     const { id, name, email, provider } = await User.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
+  }
+
+  async update(req, res) {
+    const { email, currentPassword } = req.body;
+    const user = await User.findByPk(req.userId);
+
+    if (email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'User already exists!' });
+      }
+    }
+
+    if (currentPassword && !(await user.checkPassword(currentPassword))) {
+      return res.status(401).json({ error: 'Password does not match!' });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
 
     return res.json({
       id,
---
+++ backend/src/app/middlewares/auth.js
@@ -0,0 +1,23 @@
+import jwt from 'jsonwebtoken';
+import { promisify } from 'util'; // transforma função callback em async/await
+
+import authConfig from '../../config/auth';
+
+export default async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ error: 'Token not provided!' });
+  }
+
+  const [, token] = authHeader.split(' '); // Desestruturação , (posição 1)
+
+  try {
+    const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+    req.userId = decoded.id;
+
+    return next();
+  } catch (err) {
+    return res.status(401).json({ error: 'Invalid token' });
+  }
+};
